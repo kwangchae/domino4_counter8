4195916fd24172f972f116b1990bac47c06d2673
kwangchae <kokc232@inu.ac.kr>
2025-04-16 14:36:46 +0900

    update README.md

--- counter16/bt_counter16.py
+++ counter16/bt_counter16.py
@@ -6,7 +6,7 @@ from time import sleep
 switch_pin = 25
 switch = Button(switch_pin, pull_up=True)
 
-gpio_pins = [17, 18, 27]
+gpio_pins = [17, 18, 27, 22]
 leds = []
 
 for pin in gpio_pins:
@@ -16,10 +16,12 @@ def display_binary(number):
     bit0 = number & 1        
     bit1 = (number >> 1) & 1
     bit2 = (number >> 2) & 1
+    bit3 = (number >> 3) & 1
     
     leds[0].value = bit0
     leds[1].value = bit1
     leds[2].value = bit2
+    leds[3].value = bit3
 
 counter = 0
 previous_state = False
@@ -28,7 +30,7 @@ while True:
     current_state = switch.is_pressed
         
     if current_state and not previous_state:
-        counter = (counter + 1) % 8
+        counter = (counter + 1) % 16
         display_binary(counter)
         sleep(0.2)
         
